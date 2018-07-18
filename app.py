b262ff8a3d3cce83b3374ce821cb2c1ca06856ba
traitanit-h <traitanit.h@linecorp.com>
2018-07-18 17:46:00 +0700

    reset

--- app.py
+++ app.py
@@ -55,66 +55,9 @@ def callback():
 def hello_world():
     return 'Hello World!'
 
-
-def get_nearest_bus_stop(lat, lng, address):
-    payload = {
-        'nearest_station': '{0}, {1}'.format(lat, lng),
-        'MODE': 'get_marker_all_station',
-        'nearest_station_lat': lat,
-        'nearest_station_lng': lng,
-        'keyword_station': address
-    }
-    response = requests.post(api_url, data=payload)
-    return response
-
-
-def get_arrival_data(stop_name):
-    print('stop_name: \'{0}\''.format(stop_name))
-    payload = {
-        'origin': '{0}'.format(stop_name),
-        'MODE': 'get_poly_origin'
-    }
-    response = requests.post(api_url, data=payload)
-    if response.status_code == 200:
-        json = response.json()
-        print(json)
-        text = 'สายที่กำลังจะเข้ามา: \n'
-        for number, route_data in json.items():
-            if route_data['bound'] == 'ขาออก':
-                text += 'สาย {0} รออีก {1}\n'.format(route_data['bus_line'], route_data['duration_text'])
-        return text
-    else:
-        return ''
-
-
 @handler.add(MessageEvent, message=LocationMessage)
 def handle_location_message(event):
     lat = event.message.latitude
-    lng = event.message.longitude
-    address = event.message.address
-    response = get_nearest_bus_stop(lat, lng, address)
-    reply_messages = []
-    if response.status_code == 200:
-        json = response.json()
-        for stop_id, stop_data in json.items():
-            if stop_data['radius'] < 0.1:
-                text = 'ป้ายที่ใกล้ที่สุด: {0}\n'.format(stop_data['stop_name'])
-                ### FOR DEMO
-                text += 'สายที่กำลังจะเข้ามา: {0}'.format(stop_data['bus_line_inbound'])
-
-                ### add more information
-                # text += get_arrival_data(stop_data['stop_name'])
-
-                reply_messages.append(TextSendMessage(text=text))
-                reply_messages.append(LocationSendMessage(
-                    title=stop_data["stop_name"],
-                    address=stop_data["stop_name"],
-                    latitude=stop_data["latitude"],
-                    longitude=stop_data["longitude"]
-                ))
-                break
-
-    line_bot_api.reply_message(event.reply_token, messages=reply_messages)
 
 
 if __name__ == '__main__':
